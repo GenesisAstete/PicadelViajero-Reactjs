d3dc850f6e7df3557dfb1470b73d4263dfcb37f6
fer <amontecinoscontreras@gmail.com>
2020-09-08 10:12:18 -0300

    componentes de react

---
+++ src/components/Contraseña.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contraseña = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Contraseña
---
+++ src/components/Muro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Muro = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Muro
