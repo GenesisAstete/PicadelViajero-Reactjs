b471573d2aedecdaffe7570140098eb5746a7077
fer <amontecinoscontreras@gmail.com>
2020-09-10 00:53:18 -0300

    leer firebase

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -4,19 +4,50 @@ import icono from '../img/usercian.png'
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import teal from '@material-ui/core/colors/teal';
+import firebase from 'firebase'
 
 const Publicacion = () => {
+    const [leer, setLeer] = React.useState([])
+    React.useEffect(() => {
+        const obtenerDatos = async () => {
+
+            try {
+                const db = firebase.firestore()
+                const data = await db.collection('usuarios').get()
+
+                console.log(data.docs)
+                const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+                console.log(arrayData)
+                setLeer(arrayData)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerDatos()
+
+    }, [])
     return (
         <Fragment>
-            <div className="contenedorPublicacion">
-                <div className="nombrePublicacion"> <img src={icono} alt="" /> <div >Pancho saavedra</div></div>
-                <div className="contenedorFoto">  <img src={foto1} className="imagenPublicacion" alt="" /></div>
-                <div className="textoPublicacion">texto</div>
-                <div className="botonesPublicacion">
-                    <DeleteIcon style={{ color: teal[50] }} />
-                    <EditIcon style={{ color: teal[50] }} />
-                </div>
-            </div>
+
+            {
+                leer.map(item => (
+                    <div className="contenedorPublicacion" key={item.id}>
+                        <div className="nombrePublicacion" >
+                            <img src={icono} alt="" /> <div>Pancho saavedra</div>
+                        </div>
+                        <div className="contenedorFoto">
+                            <img src={foto1} className="imagenPublicacion" alt=""></img>
+                        </div>
+                        <div className="textoPublicacion">{item.comentario}</div>
+                        <div className="botonesPublicacion">
+                            <DeleteIcon style={{ color: teal[50] }} />
+                            <EditIcon style={{ color: teal[50] }} />
+                        </div>
+
+                    </div>))
+            }
+
+
         </Fragment >
 
     )
