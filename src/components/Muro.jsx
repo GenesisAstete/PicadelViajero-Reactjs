547a1d3defb51e695081015fc9bb2ba70a61f1ae
fer <amontecinoscontreras@gmail.com>
2020-09-09 21:16:06 -0300

    caja para comentar con firebase

--- src/components/Muro.jsx
+++ src/components/Muro.jsx
@@ -4,9 +4,12 @@ import { Redirect } from 'react-router';
 import Navbar from './Navbar';
 import Publicacion from './Publicacion';
 import { Footer } from './Footer';
+import firebase from 'firebase';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Muro = () => {
-
+    const [tarea, setTarea] = React.useState("")
     const [exit, setExit] = React.useState(false)
     const cerrarSesion = () => {
         auth.signOut()
@@ -15,10 +18,48 @@ const Muro = () => {
 
             })
     }
+
+    const agregarComentario = async (e) => {
+        e.preventDefault()
+        console.log(tarea)
+        if (!tarea.trim()) {
+            console.log("esta vacio!!!")
+        }
+
+        try {
+            const db = firebase.firestore()
+            const nuevaTarea = {
+                comentario: tarea
+            }
+            const data = await db.collection("usuarios").add(nuevaTarea)
+            setTarea("")
+        } catch (error) {
+            console.log(error)
+            console.log("salio malena")
+        }
+    }
     return (
         <>
             <Fragment>
                 <Navbar />
+                <div className="contenedorFormulario" >
+                    <form onSubmit={agregarComentario} >
+                        <TextField
+                            onChange={e => setTarea(e.target.value)}
+                            value={tarea}
+                            id="outlined-multiline-static"
+                            label="Deja tu picada aquí"
+                            multiline
+                            rows={4}
+                            defaultValue="Default Value"
+                            variant="outlined"
+                        />
+                        <Button variant="contained" color="primary" type="submit">
+                            Publicar
+                    </Button>
+
+                    </form>
+                </div>
                 <Publicacion />
                 <Publicacion />
                 <Publicacion />
