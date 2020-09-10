e00e59924b784d1bb2cc748afcd7a0db8b9b68e1
pmvaldez <valdezp69@gmail.com>
2020-09-10 09:29:00 -0300

    ingreso y coleccions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.scss';
 import Login from './components/Login'
 import Registro from './components/Registro'
 import Contraseña from './components/Contraseña'
 import Muro from './components/Muro'
 import Carousel from './components/Carousel'
-
+import {auth, createUserProfileDocument} from './firebase'
 
 
 const App = () => {
+
+  const [currentUser,setCurrentUser] = useState();
+  const [login ,setLogin ] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged( async userAuth => {
+      if(userAuth) {
+        const user = await createUserProfileDocument(userAuth);
+        user.onSnapshot((snapshot) => {
+          setCurrentUser({
+              id: snapshot.id,
+              ...snapshot.data(),
+          });
+        });
+        console.log('entro' )
+        setLogin(true);
+      }
+      setCurrentUser(userAuth);
+    })
+  },[])
+
   return (
     <Router>
       <Switch>
         <Route path="/muro">
-          <Muro />
+          <Muro currentUser={currentUser}/>
         </Route>
         <Route path="/contraseña">
           <Contraseña />
