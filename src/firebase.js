eb78f3da2b53027b4a9832d6f5708a5afd94544f
GenesisAstete <genesis.astete.a@gmail.com>
2020-09-14 22:13:16 -0300

    orden codigo

--- src/firebase.js
+++ src/firebase.js
@@ -18,33 +18,3 @@ const db = firebase.firestore();
 const auth = firebase.auth();
 
 export {db, auth};
-
-/* const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({
-  promt: "select_account",
-});
-
-export const createUserProfileDocument =async (userAuth) => {
-    if(!userAuth) return;
-  
-   const userReference =  db.doc(`users/${userAuth.uid}`);
-   const snapShot =  await userReference.get();
-   if(!snapShot.exists) {
-     const {displayName, email, photoURL} = userAuth;
-     const createdAt = new Date();
-     try {
-       await userReference.set({
-         displayName,
-         email,
-         createdAt,
-         photoURL
-       })
-  
-     } catch (error) {
-        console.log(error)
-     }
-   }
-   
-   return userReference ;
-  }
-  export const signInWithGoogle = () => auth.signInWithPopup(provider); */
