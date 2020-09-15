ad81f04f39c4c2411f5b2f1c72e471028a21d343
GenesisAstete <genesis.astete.a@gmail.com>
2020-09-14 23:03:57 -0300

    formato fecha, foto y nombre post

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import foto1 from '../img/fotos1.jpg'
-import icono from '../img/usercian.png'
+/* import icono from '../img/usercian.png' */
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import teal from '@material-ui/core/colors/teal';
 import firebase from 'firebase'
+import moment from 'moment'
+import 'moment/locale/es' // Pasar a español
+
+
 
 const Publicacion = () => {
     const [leer, setLeer] = React.useState([])
@@ -32,7 +36,8 @@ const Publicacion = () => {
                 leer.map(item => (
                     <div className="contenedorPublicacion" key={item.id}>
                         <div className="nombrePublicacion" >
-                            <img src={icono} alt="" /> <div>Pancho saavedra</div>
+                         <img src={item.photoURL} alt="" width='12%'/> <div>{item.displayName}</div>
+                        <div>{ moment(item.fechaCreacion).subtract(10, 'days').calendar()}</div>
                         </div>
                         <div className="contenedorFoto">
                             <img src={foto1} className="imagenPublicacion" alt=""></img>
@@ -42,7 +47,6 @@ const Publicacion = () => {
                             <DeleteIcon style={{ color: teal[50] }} />
                             <EditIcon style={{ color: teal[50] }} />
                         </div>
-
                     </div>
                 ))
             }
