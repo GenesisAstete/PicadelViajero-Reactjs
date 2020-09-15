3b6fc1cf99c6434e109e8b3b3cee47b6923dc4f9
fer <amontecinoscontreras@gmail.com>
2020-09-15 13:02:45 -0300

    cambios de bototnes y actualizacion

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -14,30 +14,19 @@ const Publicacion = (props) => {
     console.log("en publicacion", props.filtro)
     const [leer, setLeer] = React.useState([])
     const [modoEdicion, setModoEdicion] = React.useState(false)
-
     React.useEffect(() => {
         const obtenerDatos = async () => {
-
             try {
                 const db = firebase.firestore()
-                const data = await db.collection('post').orderBy("fechaCreacion", "desc").get();
-                console.log(data.docs)
-                const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-                console.log(arrayData)
-                /*   let filtrarDatos = arrayData.filter(arrayData => arrayData.tipo === props.filtro );*/
-                setLeer(arrayData)
-                //  console.log("filtradooos", filtrarDatos)
-            } catch (error) {
-                console.log(error)
-            }
+                await db.collection('post').orderBy("fechaCreacion", "desc").onSnapshot(
+                    (snap => {
+                        const arrayData = snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+                        setLeer(arrayData);
+                    }));
+            } catch (error) { console.log(error) }
         }
         obtenerDatos()
-
-
     }, [])
-
-
-
 
     return (
         <Fragment>
@@ -52,22 +41,22 @@ const Publicacion = (props) => {
                             <img src={foto1} className="imagenPublicacion" alt=""></img>
                         </div>
                         {
-                        modoEdicion === 'true' ? 
-                            <>
-                            <div>{item.comentario}</div>
-                             <EditPost posts={item.id} dataPost={leer}/>  
-                            </>
-                             :  
-                            <div className="textoPublicacion">{item.comentario}</div>
-                            
+                            modoEdicion === 'true' ?
+                                <>
+                                    <div>{item.comentario}</div>
+                                    <EditPost posts={item.id} dataPost={leer} />
+                                </>
+                                :
+                                <div className="textoPublicacion">{item.comentario}</div>
+
                         }
-                            <div className="botonesPublicacion">
-                                <DeletePost posts={item.id} dataPost={leer}/>
-                                <IconButton aria-label="edit">
-                                    <EditIcon style={{ color: teal[50] }} onChange={() => setModoEdicion(true)}/>
-                                </IconButton>
-                            </div>
-                  
+                        <div className="botonesPublicacion">
+                            <DeletePost posts={item.id} dataPost={leer} />
+                            <IconButton aria-label="edit">
+                                <EditIcon style={{ color: teal[50] }} onChange={() => setModoEdicion(true)} />
+                            </IconButton>
+                        </div>
+
                     </div>
                 ))
             }
